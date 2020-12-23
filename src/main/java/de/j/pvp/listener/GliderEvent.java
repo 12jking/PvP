a4365cc09b6ae3f13110c3b56bff782a7411311d
12jking <12jkinghb@gmail.com>
2020-12-23 18:43:00 +0100

    update

--- src/main/java/de/j/pvp/listener/GliderEvent.java
+++ src/main/java/de/j/pvp/listener/GliderEvent.java
@@ -1,14 +1,15 @@
 package de.j.pvp.listener;
 
 import de.j.pvp.main.Main;
-import org.bukkit.Bukkit;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -25,25 +26,29 @@ public class GliderEvent implements Listener {
     public void onGlide(PlayerInteractEvent event){
         Player player = event.getPlayer();
         if (player.getInventory().getItemInHand().getType() == Material.FIREWORK){
-            player.teleport(new Location(player.getWorld(), player.getLocation().getX(), 80, player.getLocation().getZ()));
-            player.getLocation().add(new Vector(player.getLocation().getDirection().getBlockX(), -0.1, player.getLocation().getDirection().getBlockZ()));
-            gliding.add(player);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 500, 100));
-            new BukkitRunnable(){
+            if (!gliding.contains(player)){
+                player.teleport(new Location(player.getWorld(), player.getLocation().getX(), 80, player.getLocation().getZ()));
+                player.getLocation().add(new Vector(player.getLocation().getDirection().getBlockX(), -0.1, player.getLocation().getDirection().getBlockZ()));
+                gliding.add(player);
+                player.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 500, 100));
+                new BukkitRunnable(){
 
-                @Override
-                public void run() {
-                    Vector v = player.getLocation().getDirection();
-                    v.setY(-0.3);
-                    player.setVelocity(v);
-                    if (player.getLocation().getY() == 4){
-                        player.removePotionEffect(PotionEffectType.ABSORPTION);
-                        gliding.remove(player);
-                        cancel();
+                    @Override
+                    public void run() {
+                        Vector v = player.getLocation().getDirection();
+                        v.setY(-0.3);
+                        player.setVelocity(v);
+                        if (player.getLocation().getY() == 4){
+                            player.removePotionEffect(PotionEffectType.ABSORPTION);
+                            gliding.remove(player);
+                            cancel();
+                        }
+
                     }
+                }.runTaskTimer(Main.getPlugin(), 1, 1);
+            }else
+                player.sendMessage("§cOn cooldown!");
 
-                }
-            }.runTaskTimer(Main.getPlugin(), 1, 1);
         }
     }
 
